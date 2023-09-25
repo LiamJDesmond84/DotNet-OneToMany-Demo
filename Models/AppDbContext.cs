6f31660a6ebdf9229ad6c1877d652c0732bf6482
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-25 18:23:51 -0500

    Adding AppDbContext model

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_OneToMany_Demo.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=SportsORM.db");
+
+
+        // DBSet<Leagues/Teams/Players> directly connected to the DataController's static tables AND/OR Models(not sure which comes first, but tested while renaming one of the tables) - Not sure about PlayerTeam
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerTeam> PlayerTeams { get; set; }
+    }
+}
