8df2cf8ceeb7b77bc7e9f1568ef306d60c4f9636
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-25 18:22:15 -0500

    Adding Team model

---
+++ Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_OneToMany_Demo.Models
+{
+    public class Team
+    {
+
+        [Key]
+        public int TeamId { get; set; }
+        public string Location { get; set; }
+        public string TeamName { get; set; }
+        public int LeagueId { get; set; }
+        public League CurrLeague { get; set; }
+        public List<Player> CurrentPlayers { get; set; }
+        public List<PlayerTeam> AllPlayers { get; set; }
+
+    }
+}
