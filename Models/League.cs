04bec8d56f620db9a104215d7ac1afcb7ce167de
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-25 18:20:42 -0500

    Adding League model

---
+++ Models/League.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_OneToMany_Demo.Models
+{
+    public class League
+    {
+
+        [Key]
+        public int LeagueId { get; set; }
+        public string Name { get; set; }
+        public string Sport { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public List<Team> Teams { get; set; }
+
+    }
+}
