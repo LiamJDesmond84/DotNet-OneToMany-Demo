dd0403ffba35576a7b3a88e657fe586bc8b3bbbe
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-25 18:17:28 -0500

    Adding skeleton

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNet_OneToMany_Demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
