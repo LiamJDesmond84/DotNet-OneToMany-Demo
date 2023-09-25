588485487306077ecc184d170125fc6aff3fcbed
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-25 18:21:48 -0500

    Adding PlayerTeam model

---
+++ Models/PlayerTeam.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_OneToMany_Demo.Models
+{
+    public class PlayerTeam
+    {
+        [Key]
+        public int PlayerTeamId { get; set; }
+        public int PlayerId { get; set; }
+        public Player PlayerOnTeam { get; set; }
+        public int TeamId { get; set; }
+        public Team TeamOfPlayer { get; set; }
+    }
+}
