eadc5e5f456324552cba4c31bcc34e502e7985fa
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-26 20:00:15 -0500

    Adding DataController

---
+++ Controllers/DataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_OneToMany_Demo.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
