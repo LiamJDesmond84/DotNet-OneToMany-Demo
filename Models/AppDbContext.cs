af8c79423d96d6ef4369b5d4b5cfc110ff9fd647
Liam James Desmond <liamjdesmond84@gmail.com>
2023-09-26 20:08:13 -0500

    Updating AppDbContext

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -7,7 +7,8 @@ namespace DotNet_OneToMany_Demo.Models
     {
         public AppDbContext(DbContextOptions options) : base(options) { }
 
-
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlite("Data Source=SportsORM.db");
 
         // DBSet<Leagues/Teams/Players> directly connected to the DataController's static tables AND/OR Models(not sure which comes first, but tested while renaming one of the tables) - Not sure about PlayerTeam
         public DbSet<League> Leagues { get; set; }
