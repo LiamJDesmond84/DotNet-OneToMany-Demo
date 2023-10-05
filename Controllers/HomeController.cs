4477c9168bf274009ba2c9ef403622150dd84a64
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-05 17:44:06 -0500

    Adding queries

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -67,7 +67,7 @@ public class HomeController : Controller
     {
 
 
-        ViewBag.SamEvans = _context.Teams.Where(x => x.AllPlayers.Any(y => y.PlayerId.Equals(1))).ToList();
+        ViewBag.SamEvans = _context.Teams.Where(x => x.AllPlayers.Any(y => y.PlayerOnTeam.FirstName.Contains("Evans"))).ToList();
 
         return View();
     }
